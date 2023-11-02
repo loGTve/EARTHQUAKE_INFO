2243ec4a60f6fe6e0e26a647f037c0fd34889772
loGTve <logtve@naver.com>
2023-11-02 18:12:59 +0900

    지도에 마커 띄우는 코드

---
+++ src/components/map/mapMark.ts
@@ -0,0 +1,11 @@
+import {getEarthQuakeResponse} from '@/api/getEarthQuake';
+export const mapMark = async (map: any) => {
+    const markInfo = await getEarthQuakeResponse();
+    
+    console.table(markInfo);
+    for(let i = 0; i < markInfo.length; i++){
+        const [lat, lng, location] = markInfo[i];
+        console.log ( lat, lng );
+        new google.maps.Marker({position: { lat: lat, lng: lng}, map: map})
+    }
+}
