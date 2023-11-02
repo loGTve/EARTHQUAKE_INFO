fcb0826ddbb29a9a08113f403031f497e4f687bd
loGTve <logtve@naver.com>
2023-11-02 18:13:28 +0900

    환경변수

--- src/types/process-env.d.ts
+++ src/types/process-env.d.ts
@@ -5,8 +5,8 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             //이곳에 .env변수 타입 선언
-            REACT_APP_GOOGLE_MAP_API: string;
-            REACT_APP_GOOGLE_MAP_VERSION: string;
+            NEXT_PUBLIC_GOOGLE_MAP_API: string;
+            NEXT_PUBLIC_GOOGLE_MAP_VERSION: string;
             REACT_APP_EARTHQUAKE_API: string;
             REACT_APP_EARTHQUAKE_URL: string;
             REACT_APP_EARTHQUAKE_PARMETER_URL: string;
