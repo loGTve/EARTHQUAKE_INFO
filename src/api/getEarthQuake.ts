fee87f7920b2dacbe0698e0e0770b190f0a7b4ab
loGTve <logtve@naver.com>
2023-11-02 18:11:54 +0900

    지진 정보 가져오기

--- src/api/getEarthQuake.ts
+++ src/api/getEarthQuake.ts
@@ -5,15 +5,22 @@ export const getEarthQuakeResponse = async () => {
     const apiUrl = process.env.REACT_APP_EARTHQUAKE_URL;
     const apiKey = process.env.REACT_APP_EARTHQUAKE_API;
     const apiParameter = process.env.REACT_APP_EARTHQUAKE_PARMETER_URL;
-    const apiParameterDate = process.env.REACT_APP_EARTHQUAKE_PARAMETER_DATE;
+    const apiParameterDate = process.env.REACT_APP_EARTHQUAKE_PARAMETER_DATE
     
     const getEarthquakeUrl = (
         apiUrl + apiKey
         + apiParameter + pastThreeDay
         + apiParameterDate + today
-    );
+        );
 
-    let response = await axios.get(getEarthquakeUrl);
-    console.log(response.data.response.body.items);
-    return response.data;
+    const response = await axios.get(getEarthquakeUrl);
+    const itemResponse = response.data.response.body.items.item;
+    
+    let eqMarkInfo: [number, number, string][] = [];
+    
+    for(let i = 0; i < itemResponse.length; i++){
+        eqMarkInfo.push([itemResponse[i].lat, itemResponse[i].lon, itemResponse[i].loc]);
+    }
+    
+    return eqMarkInfo;
 }
