edc1dfb2af053531f5f0a759284ee7e16376158c
loGTve <logtve@naver.com>
2023-11-07 20:11:18 +0900

    remove unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import {MapComponent} from "@/components/map/initMap";
-import {TestInit} from "@/components/map/testInit";
+import {MapComponent} from "@/components/map/InitMap";
 
 export default function Home() {
   return(
     <div>
       <div>
         <MapComponent/>
-        <TestInit/>
       </div>
     </div>
   );
