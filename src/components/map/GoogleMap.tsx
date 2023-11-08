6f8f341876162ee59f335f7b01394542fdb95435
loGTve <logtve@naver.com>
2023-11-08 15:26:19 +0900

    코드 리팩토링
    코드 리팩토링 및 date.ts 위치 변경

---
+++ src/components/map/GoogleMap.tsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { useEffect, useRef } from "react";
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { mapMark } from '@/components/map/mapComponents/mapMark';
+
+const DEFAULT_CENTER = { lat: 37.603, lng: 127.094 };
+const DEFAULT_ZOOM = 6;
+
+const InitMap = () => {
+    const ref = useRef<HTMLDivElement | null>(null);
+    let map;
+    
+    useEffect(() => {
+        if (ref.current) {
+             map = new google.maps.Map(ref.current, {
+                center: DEFAULT_CENTER,
+                zoom: DEFAULT_ZOOM,
+            });
+              mapMark(map);
+            }
+    }, [ref]);
+    
+    return (
+            <div
+                ref={ref}
+                style={{ width: "800px", height: "600px" }}
+            />
+        );
+};
+
+export const GoogleMap = () => {
+    const myApiKey: string = process.env.NEXT_PUBLIC_GOOGLE_MAP_API;
+    
+    return(
+        <Wrapper apiKey={myApiKey}>
+            <InitMap/>
+        </Wrapper>
+    )
+}
